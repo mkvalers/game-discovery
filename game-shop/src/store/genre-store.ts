4cc7a8fa2ae46e4966ae1ff0d73fa4e917cea761
mkvalers <mkvalers@gmail.com>
2026-03-16 18:00:52 +0800

    Added zustand for genre state management and updated related files.

---
+++ game-shop/src/store/genre-store.ts
@@ -0,0 +1,17 @@
+import { create } from "zustand";
+
+interface GenreStore {
+  genreId: number | undefined;
+  setGenreId: (genreId: number | undefined) => void;
+}
+
+const useGenreStore = create<GenreStore>((set) => ({
+  genreId: undefined,
+
+  setGenreId: (genreId) =>
+    set(() => ({
+      genreId
+    })),
+}));
+
+export default useGenreStore;
